70ab6e8b23004193394a60603c24aec45c52e7b0
Amyhel2 <hurtadonoemy735@gmail.com>
2025-04-21 10:56:20 -0400

    Estado inicial

---
+++ application/libraries/oidc/vendor/composer/installed.php
@@ -0,0 +1,60 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '6980fe865eb38e842c00db31e058153d53375139',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '6980fe865eb38e842c00db31e058153d53375139',
+    ),
+    'jumbojett/openid-connect-php' => 
+    array (
+      'pretty_version' => 'v0.9.7',
+      'version' => '0.9.7.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2ca058133baeda85638b15faefea4b488ec9213d',
+    ),
+    'paragonie/constant_time_encoding' => 
+    array (
+      'pretty_version' => 'v2.6.3',
+      'version' => '2.6.3.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '58c3f47f650c94ec05a151692652a868995d2938',
+    ),
+    'paragonie/random_compat' => 
+    array (
+      'pretty_version' => 'v9.99.100',
+      'version' => '9.99.100.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '996434e5492cb4c3edcb9168db6fbb1359ef965a',
+    ),
+    'phpseclib/phpseclib' => 
+    array (
+      'pretty_version' => '3.0.35',
+      'version' => '3.0.35.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '4b1827beabce71953ca479485c0ae9c51287f2fe',
+    ),
+  ),
+);
